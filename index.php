717891fe318eec32284dcbf55358015e746aacb9
BuildTools <unconfigured@null.spigotmc.org>
2020-07-10 15:51:30 +0200

    aaaa

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
 
-    <title>Athen</title>
+    <title>AAA</title>
 
     <link rel="stylesheet" href="css/CSSReset.css">
     <link rel="stylesheet" href="css/style.css">
