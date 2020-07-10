329c110b57c27223bb838cfba9d78545074b624c
Fra <francyspanu01@gmail.com>
2020-07-10 13:39:46 +0200

    abbiamo cambiato il titolo

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
 
-    <title>News Letter</title>
+    <title>Athena</title>
 
     <link rel="stylesheet" href="css/CSSReset.css">
     <link rel="stylesheet" href="css/style.css">
