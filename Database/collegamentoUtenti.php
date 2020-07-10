9ebd1a31112ec59c7fd6c535a4a004f3b9edf9c6
Fra <francyspanu01@gmail.com>
2020-07-10 15:48:49 +0200

    php

--- Database/collegamentoUtenti.php
+++ Database/collegamentoUtenti.php
@@ -1,12 +1,7 @@
-<html>
-
-  <body>
-
-    <?php
-      $conn=new mysqli("104.27.130.118","prova0027","","my_prova0027");
-      if(!$conn)
-      {
-        echo "Impossibile connettersi al server".$conn->connect_error;
-        exit;
-      }
-    ?>
+<?php
+  $conn=new mysqli("104.27.130.118","prova0027","","my_prova0027");
+  if(!$conn){
+    echo "Impossibile connettersi al server".$conn->connect_error;
+    exit;
+  }
+?>
