f5710ba636b275d3995d2d99d13dfb2483680a4d
Fra <francyspanu01@gmail.com>
2020-07-10 15:49:44 +0200

    1

--- Database/collegamentoUtenti.php
+++ Database/collegamentoUtenti.php
@@ -1,5 +1,5 @@
 <?php
-  $conn=new mysqli("104.27.130.118","prova0027","","my_prova0027");
+  $conn = new mysqli("104.27.130.118","prova0027","","my_prova0027");
   if(!$conn){
     echo "Impossibile connettersi al server".$conn->connect_error;
     exit;
