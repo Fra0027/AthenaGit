22f8d675aadbe6ef7617d67231e655f823a3a5ac
Fra <francyspanu01@gmail.com>
2020-07-10 13:38:18 +0200

    asdasdasd

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+
+    <title>News Letter</title>
+
+    <link rel="stylesheet" href="css/CSSReset.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&family=Ubuntu:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.css">
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+  </head>
+  <body>
+    <header>
+      <img src="image/logo.jpg" alt="Imglogo" class="imglogo">
+      <a href="#" id="LoginRedirect">Login</a>
+    </header>
+
+    <main>
+
+    </main>
+
+    <footer>
+
+    </footer>
+
+  </body>
+</html>
