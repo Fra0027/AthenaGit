204a38261a1fa72b10bb10d122bcc975df8dc9c4
Fra <francyspanu01@gmail.com>
2020-07-11 12:03:47 +0200

    asd

--- pageuser.php
+++ pageuser.php
@@ -2,11 +2,32 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+
+    <title>Athena</title>
+
+    <link rel="stylesheet" href="css/CSSReset.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&family=Ubuntu:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.css">
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script src="js/GivePost.js"></script>
+
   </head>
   <body>
-    <?php
-      echo "Ti sei loggato con successo";
-    ?>
+    <header>
+      <img src="images/logo.jpg" alt="Imglogo" class="imglogo">
+      <a href="login.php" id="userlogin"><?php $username ?></a>
+    </header>
+
+    <main>
+
+    </main>
+
+    <footer>
+
+    </footer>
+
   </body>
 </html>
