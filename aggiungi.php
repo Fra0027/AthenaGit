7bdad22e43b95b86aa2e0c915711346f38c3652d
Dilan <sykudilan@gmail.com>
2020-07-12 00:00:27 +0200

    Creata pagina Aggiungi , leggere i commenti ad inizio pagina

--- aggiungi.php
+++ aggiungi.php
@@ -1,5 +1,16 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
+
+            <!-- Qui secondo me dobbiamo fare un controllo , non lo so fare ma titlepost
+           <?php
+            /*   if($_SESSION["logged"] = true;)
+              {
+                Allora puoi procedere con questo sito
+              } */
+             ?>
+             Perché senno possono semplicemente digitare nell'url il nome del codice aggiungi e lo fanno senza essere loggati
+             Capì? -->
+
   <head>
     <meta charset="utf-8">
 
@@ -12,7 +23,6 @@
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.css">
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-    <script src="js/GivePost.js"></script>
 
   </head>
   <body>
@@ -29,7 +39,20 @@
       </div>
 
     </header>
-
+    <div class="box">
+      <br><br><br><br><br><br><br><br><br> Ndo cazzo sta sto box
+      <form action="" method="post">
+        Inserire url immagine <br>
+        <input type="text" name="foto" placeholder="Url foto" /> <br>
+        Inserire un titolo <br>
+        <input type="text" name="titolo" placeholder="Titolo" /> <br>
+        Inserire un sottotitolo <br>
+        <input type="text" name="stitolo" placeholder="Sotto titolo" /> <br>
+        Inserire il post <br>
+        <textarea name="post" placeholder="Post" /></textarea> <br>
+        <input type="submit" value="Pubblica" /> <br>
+      </form>
+    </div>
     <main>
 
     </main>
