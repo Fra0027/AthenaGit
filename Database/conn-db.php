69c8cc382d27dcbab216d9a95d54a834aba3e9cf
Fra <francyspanu01@gmail.com>
2020-07-11 09:55:18 +0200

    p

--- Database/conn-db.php
+++ Database/conn-db.php
@@ -2,7 +2,7 @@
 $dbServername="localhost";
 $dbUsername="root";
 $dbPassword="";
-$dbName="utenti_athena";
+$dbName="utenti";
 $conn = mysqli_connect($dbServername,$dbUsername,$dbPassword,$dbName);
 if (mysqli_connect_errno())
 {
--- insertpost.php
+++
@@ -1,3 +0,0 @@
-<?php
-    include ("database/verify.php");
-?>
