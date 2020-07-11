6fb5deb71299042a507659aa0823e29d4e3859e0
Fra <francyspanu01@gmail.com>
2020-07-11 11:23:47 +0200

    login funzioannte

--- Database/verify.php
+++ Database/verify.php
@@ -1,32 +1,17 @@
 <?php
-  include ("conn-db.php");
-  $email = $_POST['email'];   /*mysqli_real_escape_string($conn, $_POST["email"]);*/
-  $password = $_POST['password'];   /*mysqli_real_escape_string($conn, $_POST["password"]);*/
-
-  if (empty($email) || empty($password)) {
-    echo "email or password error";
-    header('Location: login.php');
-    exit();
+  session_start(); // open session
+  include("conn-db.php"); // connection db
+  $username = $_POST["username"];
+  $password = $_POST["password"];
+  $sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+  $result = $conn->query($sql);
+  if(mysqli_num_rows($result)==0){
+    $_SESSION["logged"] = false;
+    echo "non sei registrato";
+    $conn->close();
   }
   else{
-    $sql = "SELECT * FROM user WHERE email = '$email' AND password = '$password' ";
-    $result = mysqli_query($conn,$sql);
-    //controll how many result
-    if ($result->num_rows > 0) {
-      // control email and password
-      while($row = $result->fetch_assoc()) {
-        if ($email=$row["email"] || $password=$row["password"]) {
-          echo "connessione riuscita";
-          header('Location: insertpost.php');
-          echo $email;
-          echo $password;
-        }
-      }
-    } else {
-      echo "email or password error";
-      $conn->close();
-    }
-
+    $_SESSION["logged"] = true;
+    header("location:../insertpost.php");
   }
-
 ?>
--- login.php
+++ login.php
@@ -1,31 +1,26 @@
 <html>
-
 	<head>
-
 		<title>Login</title>
 		<link rel="stylesheet" href="css/CSSReset.css">
 		<link rel="stylesheet" href="css/login.css">
-
 	</head>
-
 	<body>
-
 		<div class="box">
 			<div class="boxTitolo">
 				Login
 			</div>
 			<div class="boxInterno">
-				<form action="insertpost.php" method="post">
-					Inserire Email	<br>
-					<input type="text" name="email" placeholder="Email" />	<br>
-					Inserire password	<br>
-					<input type="text" name="password" placeholder="Password" />	<br>
-					<input type="submit" name="login" value="Login" />	<br>
+				<?php
+				  session_start(); // come sempre prima cosa, aprire la sessione
+				  include("database/db_conn.php"); // includere la connessione al database
+				?>
+				<h2>Login</h2>
+				<form method="post" action="database/verify.php">
+				  <p>Username</p><input type="text" name="username" >
+				  <p>Password</p><input type="text" name="password">
+				  <p><button>Accedi</button></p>
 				</form>
 			</div>
 		</div>
-
-
 	</body>
-
 </html>
