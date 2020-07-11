8823a3cef169e4d418c3ff8b0a08ea2729950147
Fra <francyspanu01@gmail.com>
2020-07-11 14:25:10 +0200

    fix injection

--- Database/verify.php
+++ Database/verify.php
@@ -1,17 +1,27 @@
 <?php
   session_start(); // open session
   include("conn-db.php"); // connection db
-  $username = $_POST["username"];
-  $password = $_POST["password"];
-  $sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
-  $result = $conn->query($sql);
-  if(mysqli_num_rows($result)==0){
-    $_SESSION["logged"] = false;
-    echo "non sei registrato";
-    $conn->close();
+
+  $username = mysqli_real_escape_string($conn, $_POST["username"]);
+  $password = mysqli_real_escape_string($conn, $_POST["password"]);
+
+  if (empty($username) || empty($password)) {
+    header("Location: ../login.php");
+    exit();
   }
   else{
-    $_SESSION["logged"] = true;
-    header("location:../pageuser.php");
+    $sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+    $result = $conn->query($sql);
+
+    if(mysqli_num_rows($result)==0){
+      $_SESSION["logged"] = false;
+      echo "non sei registrato";
+      $conn->close();
+    }
+    else{
+      $_SESSION["logged"] = true;
+      header("location:../pageuser.php");
+    }
   }
+
 ?>
